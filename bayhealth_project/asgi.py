b5e32f2b6cb4c04e21f846363a11e2b57b4ff89d
00peteromondi <00peteromondi@gmail.com>
2026-04-01 10:27:51 +0300

    Improved alot. Working via Rails

--- bayhealth_project/asgi.py
+++ bayhealth_project/asgi.py
@@ -6,12 +6,12 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+django_asgi_app = get_asgi_application()
+
 from ambulance.routing import websocket_urlpatterns as ambulance_ws
 from core.routing import websocket_urlpatterns as core_ws
 from hospital.routing import websocket_urlpatterns as hospital_ws
 from telemedicine.routing import websocket_urlpatterns as telemedicine_ws
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
