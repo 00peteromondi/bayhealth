b6655cb1a412271dd013f453268e8edcd86c608b
00peteromondi <00peteromondi@gmail.com>
2026-04-01 10:17:24 +0300

    Improved alot. Working via Rails

--- bayhealth_project/asgi.py
+++ bayhealth_project/asgi.py
@@ -1,5 +1,7 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bayhealth_project.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -8,9 +10,6 @@ from ambulance.routing import websocket_urlpatterns as ambulance_ws
 from core.routing import websocket_urlpatterns as core_ws
 from hospital.routing import websocket_urlpatterns as hospital_ws
 from telemedicine.routing import websocket_urlpatterns as telemedicine_ws
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bayhealth_project.settings")
 
 django_asgi_app = get_asgi_application()
 
